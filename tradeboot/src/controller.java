fc32e21e919558267258688981dc59ddc7fc4fab
MagischerWolf05 <gianwilhelm3@gmail.com>
2021-05-20 16:00:23 +0200

    added data to drop down menus

--- tradeboot/src/controller.java
+++ tradeboot/src/controller.java
@@ -1,11 +1,9 @@
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
-import java.util.SimpleTimeZone;
 import java.util.TimeZone;
 
 
@@ -34,18 +32,58 @@ public class controller{
     Label stoploos;
 
     ObservableList<String> timezonearray = FXCollections.observableArrayList();
+    ObservableList<String> optionarray = FXCollections.observableArrayList();
+    ObservableList<String> buyOperatorarray = FXCollections.observableArrayList();
+
+    String buyoptionstring = "";
+    String selloptionstring = "";
+
     public void initialize() {
+    compareOption();
     AddTimeZone();
-    Timezonemenu.getItems().addAll(timezonearray);
+    Optiondropdown();
+    buyoption2options();
+    selloption2options();
+    buyOperator.getItems().addAll(this.buyOperatorarray);
+    selloperator.getItems().addAll(this.buyOperatorarray);
+    Timezonemenu.getItems().addAll(this.timezonearray);
+    buyOption.getItems().addAll(this.optionarray);
+    buyoption2.getItems().addAll(this.optionarray);
+    selloption.getItems().addAll(this.optionarray);
+    selloption2.getItems().addAll(this.optionarray);
 
     }
 
     public void AddTimeZone(){
         for (int i = 0; i < TimeZone.getAvailableIDs().length; i++) {
-            timezonearray.add(TimeZone.getAvailableIDs()[i]);
+            this.timezonearray.add(TimeZone.getAvailableIDs()[i]);
         }
     }
-
-
-
+    public void Optiondropdown()
+    {
+        this.optionarray.add("14 Day RSI");
+        this.optionarray.add("Avg Preis Stunde");
+        this.optionarray.add("Avg Preis 5 Stunden");
+        this.optionarray.add("Avg Preis 10 Tage");
+        this.optionarray.add("Avg Preis 24h");
+        this.optionarray.add("Avg Preis 7 Tage");
     }
+    public void buyoption2options(){
+        buyoption2.setEditable(true);
+        buyoption2.setOnAction((Event ev) -> {
+           this.buyoptionstring = buyoption2.getSelectionModel().getSelectedItem().toString();
+        });
+    }
+    public void selloption2options(){
+        selloption2.setEditable(true);
+        selloption2.setOnAction((Event ev) -> {
+            this.selloptionstring = selloption2.getSelectionModel().getSelectedItem().toString();
+        });
+    }
+    public void compareOption()
+    {
+        this.buyOperatorarray.add("<");
+        this.buyOperatorarray.add("=");
+        this.buyOperatorarray.add(">");
+    }
+}
